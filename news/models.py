48566ddf8bc8d1bfb8271dd4e68cdcd06c758fd2
Стокалюк Никита Алексеевич <19862468@sigma.sbrf.ru>
2022-09-02 14:06:29 +0300

    init simple site per project

---
+++ news/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(blank=True, upload_to='photo/%Y/%m/%d', verbose_name='Фото')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано?')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at', 'title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
---
+++ news/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import News
+
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news, 'title': 'Список новостей'
+    }
+    return render(request, 'news/index.html', context)
